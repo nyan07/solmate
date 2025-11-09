ab42a6c068e292073db299eda3e5f63b475ac9cd
Dani Araujo <daniela.araujo@flix.com>
2025-11-09 16:52:02 +0100

    minor imporvements on places list

--- src/places/components/PlaceItem.tsx
+++ src/places/components/PlaceItem.tsx
@@ -20,7 +20,7 @@ export const PlaceItem = ({ place, distance }: PlaceItemProps) => {
                 <div className='flex gap-2 relative items-center -mt-1'>
                     <span className='absolute -top-2 right-0'>{distance}</span>
                     {/* {place.photoUrl && <img src={place.photoUrl} className='w-12 h-12 rounded-full bg-gray-300' />} */}
-                    <h4 className='text-neutral-dark grow-1 text-lg flex'>
+                    <h4 className='text-neutral-dark grow-1 text-lg inline'>
                         <span className="pt-1">{place.displayName}</span>
                         <span className="pl-2"><PlaceTypeIcon type={place.primaryType} className="w-4 h-4 inline" /></span>
                     </h4>
