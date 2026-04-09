304ab50cd6a89226fc48add3c4b35ada14a41ca6
Dani Araujo <daniela.araujo@flix.com>
2026-04-09 10:48:56 +0200

    chore: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./i18n";
 
 const queryClient = new QueryClient();
@@ -14,5 +16,7 @@ createRoot(document.getElementById("root")!).render(
                 <App />
             </BrowserRouter>
         </QueryClientProvider>
+        <Analytics />
+        <SpeedInsights />
     </StrictMode>
 );
